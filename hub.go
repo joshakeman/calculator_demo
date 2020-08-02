d497525316ec733bd2dcd18fbba90c2bcab574cc
Josh Akeman <joshakeman@gmail.com>
2020-08-02 15:29:40 -0400

    first commit

---
+++ hub.go
@@ -0,0 +1,67 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"container/list"
+)
+
+// Hub maintains the set of active clients and broadcasts messages to the
+// clients.
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+
+	// Maintain list of last ten messages.
+	latest *list.List
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+		latest:     list.New(),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if h.clients[client] {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+			if h.latest.Len() < 10 {
+				h.latest.PushFront(message)
+			} else if h.latest.Len() == 10 {
+				h.latest.Remove(h.latest.Back())
+				h.latest.PushFront(message)
+			}
+		}
+	}
+}
